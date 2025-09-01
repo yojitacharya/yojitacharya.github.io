9a1103f9e3e922aae18abe3a484f697c6f784147
yojit acharya <151233809+yojitacharya@users.noreply.github.com>
2025-08-31 22:15:16 -0500

    Update server.js

--- Chameleon/src/server.js
+++ Chameleon/src/server.js
@@ -14,10 +14,10 @@ const io = socketIO(server, {
 
 app.use(cors());
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 // Game state management
@@ -444,6 +444,7 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Chameleon Game Server running on port ${PORT}`);
 });
+
 
 
 
