0c8ed1498cb0125548dcc915dc5eb92e5f09c63a
Spotswa Acharya <spotswaacharya@gmail.com>
2025-09-17 19:52:55 -0500

    idk

--- Chameleon/server.js
+++ Chameleon/server.js
@@ -515,6 +515,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
-    console.log(`Chameleon Game Server running on port ${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Chameleon Game Server running on port ${PORT}`);
 });
